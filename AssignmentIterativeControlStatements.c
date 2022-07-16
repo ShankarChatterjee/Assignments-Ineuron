a155f0c52af760220eab308874f606521e3d0f60
ShankarChatterjee <108972563+ShankarChatterjee@users.noreply.github.com>
2022-07-16 19:44:06 +0530

    Add files via upload

---
+++ AssignmentIterativeControlStatements.c
@@ -0,0 +1,92 @@
+//WAP to print MySir G on the Screen.
+/*#include <stdio.h>
+int main(){
+int i=1,n;
+printf("Enter a number\n");
+scanf("%d",&n);
+while(i<=n){
+    printf("MySirG\n");
+    i++;
+}
+return 0;
+}*/
+//WAP to print the first 10 natural numbers
+/*#include <stdio.h>
+int main(){
+    int i=1;
+    while(i<=10){
+        printf("%d\n",i);
+        i++;
+    }
+    return 0;
+}*/
+//WAP to print the first 10 natural numbers in reverse order
+/*#include <stdio.h>
+int main(){
+    for(int i=10;i>=1;i--){
+        printf("%d\n",i);
+
+    }
+    return 0;
+}*/
+//WAP to print first 10 odd natural numbers
+/*#include <stdio.h>
+int main(){
+    for(int i=1;i<20;i++){
+        if(i%2==1) printf("%d\n",i);
+    }
+    return 0;
+}*/
+//WAP to print the first 10 odd natural numbers in reverse order.
+/*#include <stdio.h>
+int main(){
+    for(int i=20;i>=1;i--){
+        if(i%2==1) printf("%d\n",i);
+    }
+    return 0;
+}*/
+//WAP to print the first 10 even natural numbers
+/*#include <stdio.h>
+int main(){
+    for(int i=1;i<=20;i++){
+        if(i%2==0) printf("%d\n",i);
+    }
+    return 0;
+}*/
+//WAP to print the first 10 even natural numbers in reverse order
+/*#include <stdio.h>
+int main(){
+    for(int i=20;i>=2;i--){
+        if(i%2==0) printf("%d\n",i);
+    }
+    return 0;
+}*/
+//WAP to print square of the first 10 natural numbers
+/*#include <stdio.h>
+int main(){
+    int i=1;
+    while(i<=10){
+        printf("%d\n",i*i);
+        i++;
+    }
+}*/
+//WAP to print cubes of the first 10 natural numbers
+/*#include <stdio.h>
+int main(){
+    int i=1;
+    while(i<=10){
+        printf("%d\n",i*i*i);
+        i++;
+    }
+}*/
+//WAP to print a table of 5
+/*#include <stdio.h>
+int main(){
+    int table = 5,i;
+    for(i=1;i<=10;i++){
+        printf("%d X %d = %d\n",table,i,table*i);
+    }
+    return 0;
+}*/
+
+
